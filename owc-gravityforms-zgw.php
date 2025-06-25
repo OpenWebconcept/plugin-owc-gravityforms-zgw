9b5846f7acf564e90dee2c8ee8aaa88c139d0ceb
Mike van den Hoek <mike@yard.nl>
2025-06-25 16:08:44 +0200

    (refactor): init plugin on 'after_setup_theme' hook

--- owc-gravityforms-zgw.php
+++ owc-gravityforms-zgw.php
@@ -1,9 +1,12 @@
 <?php
+
 /**
  * OWC GravityForms ZGW.
  *
  * @package OWC_GravityForms_ZGW
+ *
  * @author  Yard | Digital Agency
+ *
  * @since   1.0.0
  *
  * Plugin Name:       OWC | GravityForms ZGW
@@ -24,27 +27,27 @@ declare (strict_types = 1);
 /**
  * Exit when accessed directly.
  */
-if ( ! defined( 'ABSPATH' )) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 
-const OWC_GRAVITYFORMS_ZGW_VERSION             = '1.0.0';
+const OWC_GRAVITYFORMS_ZGW_VERSION = '1.0.0';
 const OWC_GRAVITYFORMS_ZGW_REQUIRED_WP_VERSION = '6.7';
-const OWC_GRAVITYFORMS_ZGW_FILE                = __FILE__;
+const OWC_GRAVITYFORMS_ZGW_FILE = __FILE__;
 
-define( 'OWC_GRAVITYFORMS_ZGW_DIR_PATH', plugin_dir_path( OWC_GRAVITYFORMS_ZGW_FILE ) );
-define( 'OWC_GRAVITYFORMS_ZGW_PLUGIN_URL', plugins_url( '/', OWC_GRAVITYFORMS_ZGW_FILE ) );
-const OWC_GRAVITYFORMS_ZGW_SETTINGS_PREFIX        = 'owc-gf-zgw';
+define('OWC_GRAVITYFORMS_ZGW_DIR_PATH', plugin_dir_path(OWC_GRAVITYFORMS_ZGW_FILE));
+define('OWC_GRAVITYFORMS_ZGW_PLUGIN_URL', plugins_url('/', OWC_GRAVITYFORMS_ZGW_FILE));
+const OWC_GRAVITYFORMS_ZGW_SETTINGS_PREFIX = 'owc-gf-zgw';
 const OWC_GRAVITYFORMS_ZGW_ADD_ON_SETTINGS_PREFIX = 'owc-gf-zgw-add-on';
-const OWC_GRAVITYFORMS_ZGW_PLUGIN_SLUG            = 'owc-gravityforms-zgw';
+const OWC_GRAVITYFORMS_ZGW_PLUGIN_SLUG = 'owc-gravityforms-zgw';
 
 const OWC_GRAVITYFORMS_ZGW_TRANSIENT_KEY_FAILED_SUBMISSION = 'zgw_submission_zaak_failed_message';
-const OWC_GRAVITYFORMS_ZGW_TRANSIENT_KEY_CREATED_ZAAK      = 'zgw_created_zaak';
-const OWC_GRAVITYFORMS_ZGW_LOGGER_DEFAULT_MAX_FILES        = 7;
+const OWC_GRAVITYFORMS_ZGW_TRANSIENT_KEY_CREATED_ZAAK = 'zgw_created_zaak';
+const OWC_GRAVITYFORMS_ZGW_LOGGER_DEFAULT_MAX_FILES = 7;
 
 $autoload = __DIR__ . '/vendor/autoload.php';
 
-if (file_exists( $autoload )) {
+if (file_exists($autoload)) {
 	require_once $autoload;
 }
 
@@ -52,7 +55,7 @@ require_once __DIR__ . '/src/autoload.php';
 require_once __DIR__ . '/src/Bootstrap.php';
 
 add_action(
-	'plugins_loaded',
+	'after_setup_theme',
 	function () {
 		$init = new OWCGravityFormsZGW\Bootstrap();
 	}
