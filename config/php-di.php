1273bf8e5c6fa9323da276c40ba019118feb12d8
Robert Bossaert <contact@robertbossaert.com>
2025-10-10 09:46:09 +0200

    chore: allow multiple same registers (#6)

--- config/php-di.php
+++ config/php-di.php
@@ -9,53 +9,22 @@ use OWCGravityFormsZGW\Settings\Settings;
 return array(
 	/**
 	 * Suppliers
+	 *
+	 * Best kept in sync with:
+	 * https://github.com/OpenWebconcept/owc-zgw-api/blob/main/src/WordPress/SettingsProvider.php#L50
 	 */
-	'suppliers'                   => array(
-		'openzaak'   => 'OpenZaak',
-		'decos-join' => 'DecosJOIN',
-		'rx-mission' => 'RxMission',
-		'xxllnc'     => 'XXLLNC',
-		'procura'    => 'Procura',
+	'suppliers'                  => array(
+		'openzaak'  => 'OpenZaak',
+		'xxllnc'    => 'XXLLNC',
+		'rxmission' => 'RxMission',
+		'decosjoin' => 'Decos JOIN',
+		'procura'   => 'Procura',
 	),
-
-	/**
-	 * Specific client settings.
-	 */
-	'oz.enabled'                  => function (Container $container ) {
-		return (bool) $container->make( 'zgw.get-configured-client', array( 'openzaak' ) );
-	},
-	'oz.api-client-settings'      => function (Container $container ) {
-		return $container->make( 'zgw.get-configured-client', array( 'openzaak' ) );
-	},
-	'xxllnc.enabled'              => function (Container $container ) {
-		return (bool) $container->make( 'zgw.get-configured-client', array( 'xxllnc' ) );
-	},
-	'xxllnc.api-client-settings'  => function (Container $container ) {
-		return $container->make( 'zgw.get-configured-client', array( 'xxllnc' ) );
-	},
-	'dj.enabled'                  => function (Container $container ) {
-		return (bool) $container->make( 'zgw.get-configured-client', array( 'decosjoin' ) );
-	},
-	'dj.api-client-settings'      => function (Container $container ) {
-		return $container->make( 'zgw.get-configured-client', array( 'decosjoin' ) );
-	},
-	'rx.enabled'                  => function (Container $container ) {
-		return (bool) $container->make( 'zgw.get-configured-client', array( 'rxmission' ) );
-	},
-	'rx.api-client-settings'      => function (Container $container ) {
-		return $container->make( 'zgw.get-configured-client', array( 'rxmission' ) );
-	},
-	'procura.enabled'             => function (Container $container ) {
-		return (bool) $container->make( 'zgw.get-configured-client', array( 'procura' ) );
-	},
-	'procura.api-client-settings' => function (Container $container ) {
-		return $container->make( 'zgw.get-configured-client', array( 'procura' ) );
-	},
 
 	/**
 	 * Generic client settings.
 	 */
-	'zgw.get-configured-client'   => function (Container $container, string $type, string $name ) {
+	'zgw.get-configured-client'  => function (Container $container, string $type, string $name ) {
 		$clients = $container->make( 'zgw.api.settings', array( 'zgw-api-configured-clients' ) ) ?: array();
 		$clients = array_filter(
 			$clients,
@@ -67,28 +36,28 @@ return array(
 
 		return is_array( $client ) && 0 < count( $client ) ? $client : array();
 	},
-	'zgw.api-configured-clients'  => function (Container $container ) {
+	'zgw.api-configured-clients' => function (Container $container ) {
 		return $container->make( 'zgw.api.settings', array( 'zgw-api-configured-clients' ) );
 	},
-	'zgw.api.settings'            => function (Container $container, string $type, string $name ) {
+	'zgw.api.settings'           => function (Container $container, string $type, string $name ) {
 		return Settings::make( 'zgw_api_settings' )->get( $name );
 	},
-	'zgw.rsin'                    => function (Container $container ) {
+	'zgw.rsin'                   => function (Container $container ) {
 		return $container->make( 'zgw.addon.settings', array( 'owc-gf-zgw-add-on-organization-rsin' ) );
 	},
-	'zgw.addon.settings'          => function (Container $container, string $type, string $name ) {
+	'zgw.addon.settings'         => function (Container $container, string $type, string $name ) {
 		return Settings::make( 'gravityformsaddon_owc-gravityforms-zgw_settings' )->get( $name );
 	},
-	'digid.current_user_bsn'      => DigiD::make()->bsn(),
+	'digid.current_user_bsn'     => DigiD::make()->bsn(),
 
 	/**
 	 * ZGW error logging.
 	 */
-	'message.logger.active'       => function (Container $container ) {
+	'message.logger.active'      => function (Container $container ) {
 		return (bool) $container->make( 'zgw.addon.settings', array( 'owc-gf-zgw-add-on-logging-enabled' ) );
 	},
-	'message.logger.path'         => sprintf( '%s/owc-zgw-log.json', wp_get_upload_dir()['basedir'] ),
-	'message.logger'              => function (Container $container ) {
+	'message.logger.path'        => sprintf( '%s/owc-zgw-log.json', wp_get_upload_dir()['basedir'] ),
+	'message.logger'             => function (Container $container ) {
 		$logger   = new \Monolog\Logger( 'owc_zgw_log' );
 		$maxFiles = apply_filters( 'owcgfzgw::logger/rotating_filer_handler_max_files', OWC_GRAVITYFORMS_ZGW_LOGGER_DEFAULT_MAX_FILES );
 
@@ -104,7 +73,7 @@ return array(
 		return $logger;
 	},
 
-	'logger.zgw'                  => function (Container $container ) {
+	'logger.zgw'                 => function (Container $container ) {
 		return new \OWCGravityFormsZGW\LoggerZGW( $container->get( 'message.logger' ) );
 	},
 );
