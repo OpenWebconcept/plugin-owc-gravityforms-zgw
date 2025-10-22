85acc0f95b251c95e3ef86a791293ae252b655ad
Robert Bossaert <contact@robertbossaert.com>
2025-10-22 12:54:14 +0200

    chore: update readme

--- src/Transactions/TransactionPostType.php
+++ src/Transactions/TransactionPostType.php
@@ -70,7 +70,7 @@ class TransactionPostType
 		}
 
 		$class   = 'notice notice-info';
-        $message = __( 'Overzicht van zaaksysteemtransacties vanuit Gravity Forms formulieren welke gekoppeld zijn met een zaaksysteem.', 'Transaction status', 'owc-gravityforms-zgw' );
+        $message = __( 'Overzicht van zaaksysteemtransacties vanuit Gravity Forms formulieren welke gekoppeld zijn met een zaaksysteem.', 'Transactie status', 'owc-gravityforms-zgw' );
 
         printf(
 			'<div class="notice notice-%1$s"><p>%2$s</p></div>',
