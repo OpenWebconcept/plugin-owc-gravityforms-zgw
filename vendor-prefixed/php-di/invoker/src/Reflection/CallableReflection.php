790447ae3f63cc16cd51a82feb4fd63d202f6d57
Mike van den Hoek <mike@yard.nl>
2026-01-12 21:50:48 +0100

    feat: update deps, workflows and dependabot config

--- vendor-prefixed/php-di/invoker/src/Reflection/CallableReflection.php
+++ vendor-prefixed/php-di/invoker/src/Reflection/CallableReflection.php
@@ -2,7 +2,7 @@
 /**
  * @license MIT
  *
- * Modified by plugin on 06-January-2026 using {@see https://github.com/BrianHenryIE/strauss}.
+ * Modified by plugin on 12-January-2026 using {@see https://github.com/BrianHenryIE/strauss}.
  */ declare(strict_types=1);
 
 namespace OWCGravityFormsZGW\Vendor_Prefixed\Invoker\Reflection;
